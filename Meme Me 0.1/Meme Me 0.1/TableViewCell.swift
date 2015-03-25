a674cd0f14169178fa8455976345669cf48ceb9a
tgcbraun <thiago@gcbraun.com.br>
2015-03-25 08:15:20 -0300

    Committed by Udacity

---
+++ Meme Me 0.1/Meme Me 0.1/TableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewCell.swift
+//  Meme Me 0.1
+//
+//  Created by Thiago Graça Couto Braun on 3/24/15.
+//  Copyright (c) 2015 GCBraun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewCell: UITableViewCell {
+ 
+
+    @IBOutlet weak var tableImageView: UIImageView!
+    @IBOutlet weak var textOne: UILabel!
+    @IBOutlet weak var textTwo: UILabel!
+    
+    
+}
