965701d02eefffb09b2599dd3bc0de1bb6ef7b00
tgcbraun <thiago@gcbraun.com.br>
2015-03-25 08:15:18 -0300

    Committed by Udacity

---
+++ Meme Me 0.1/Meme Me 0.1/CollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionViewCell.swift
+//  Meme Me 0.1
+//
+//  Created by Thiago Graça Couto Braun on 3/23/15.
+//  Copyright (c) 2015 GCBraun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var textOne: UILabel!
+    
+    @IBOutlet weak var textTwo: UILabel!
+
+    @IBOutlet weak var collectionImageView: UIImageView!
+    
+
+   
+    
+}
