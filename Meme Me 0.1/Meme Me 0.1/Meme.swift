6e3654bbdc1f0dd80860fee341a5b310dbf62b32
tgcbraun <thiago@gcbraun.com.br>
2015-03-25 08:15:20 -0300

    Committed by Udacity

---
+++ Meme Me 0.1/Meme Me 0.1/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  Meme Me 0.1
+//
+//  Created by Thiago Graça Couto Braun on 3/23/15.
+//  Copyright (c) 2015 GCBraun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var text1: String
+    var text2: String
+    var image: UIImage
+    var memedImage: UIImage
+    
+}
